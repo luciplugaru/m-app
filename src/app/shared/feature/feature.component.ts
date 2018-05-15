6e93061d89b1666632b889ae8ea01e0a8dd8167e
Lucia P <lucia.plugaru@gmail.com>
2018-05-15 22:49:56 +0300

    implemented mapillary js viewer

--- src/app/shared/feature/feature.component.ts
+++ src/app/shared/feature/feature.component.ts
@@ -1,18 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import * as Mapillary from 'mapillary-js';
 
 import { ExtendedFeature } from '../interfaces';
+import { mClientId } from '../settings';
 
 @Component({
   selector: 'shared-feature',
   templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.css']
+  styleUrls: [ './feature.component.css' ]
 })
-export class FeatureComponent implements OnInit {
+export class FeatureComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() feature: ExtendedFeature;
   @Input() shortVersion: boolean;
+  viewer: any;
   
   ngOnInit() {
     console.log('FeatureComponent init');
+  }
+  
+  initMapillaryViewer() {
+    this.viewer = new Mapillary.Viewer(
+      'viewer',
+      mClientId,
+      this.feature.imageKey
+    );
+  }
+  
+  ngAfterViewInit() {
+    this.initMapillaryViewer();
+  }
+  
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.feature && changes.feature.previousValue
+        && changes.feature.currentValue.imageKey !== changes.feature.previousValue.imageKey) {
+      this.initMapillaryViewer();
+    }
   }
   
 }
